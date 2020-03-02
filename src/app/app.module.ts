7b46bcf8b137b8ee90f8579bd87b534e04981287
Mihir Panchal <pmihir98512@gmail.com>
2020-03-02 22:46:09 +0530

    Validation implmented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+      this.registerForm = this.formBuilder.group({
+        name: ['',Validators.required],
+        email:['',[Validators.required,Validators.email]],
+        phoneNumber: ['', [Validators.required, Validators.minLength(10)]],
+        password:['',Validators.required,Validators.minLength(6)],
+        confirmPassword:['',Validators.required]
+      })
   }
+
+  get f(){
+    return this.registerForm.controls;
+  }
+
+  onSubmit(){
+    this.submitted = true;
+  }
+  
+  // matchPassword(){
+  //   if(this.registerForm.controls.password == this.registerForm.controls.confirmPassword){
+  //     console.log("password match");
+  //   }
+  // }
 
 }
