094517a42deaad9b4f03a25cf16c3cb8a17f8f69
Mihir Panchal <pmihir98512@gmail.com>
2020-06-19 11:54:53 +0530

    all functionality implemented in store module

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -4,6 +4,7 @@ import { Route } from '@angular/compiler/src/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { PagerService } from './pager.service';
 import { StoreService } from './store.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-store',
@@ -15,34 +16,42 @@ export class StoreComponent implements OnInit {
   productArray: any[];
   categoryFilterArray: any[] = [];
   selectedCategory: any[] = [];
+  selectedBrand: any[] = [];
   productDisplay: any[] = [];
   hash = new Object();
   objectKeys = Object.keys;
-  rangeValues: number[] = [1, 10000];
+  rangeValues: number[] = [1, 100000];
   displayUI: any[] = [];
   sortCategory = [
-    { id: 0, label: "" },
+    { id: 0, label: "Select" },
     { id: 1, label: "High To Low" },
     { id: 2, label: "Low To High" }
   ];
   selected: number = 0;
   pager: any = {};
+  minRange: number = 1;
+  maxRange: number = 100000;
+  resultTemp: any[];
+  brandDict = new Object();
 
-  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router, private pagerService: PagerService, private storeService: StoreService) { }
+  constructor(private dataService: DataService, private spinner: NgxSpinnerService, private route: ActivatedRoute, private router: Router, private pagerService: PagerService, private storeService: StoreService) { }
 
   ngOnInit(): void {
 
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.categorySelected = params.get('category');
       this.getProductData();
+
     });
   }
   getProductData() {
+    this.spinner.show();
     this.storeService.getElectronicsCollection(this.categorySelected).subscribe(
       (success) => {
         console.log(success);
         this.productArray = success;
         this.displayUI = this.productArray;
+        this.resultTemp = this.productArray;
         this.productDisplay = this.productArray;
         for (let i = 0; i < this.productArray.length; i++) {
           let temp = this.productArray[i].category;
@@ -53,7 +62,17 @@ export class StoreComponent implements OnInit {
             this.hash[temp] += 1;
           }
         }
+        for (let j = 0; j < this.productArray.length; j++) {
+          let tempBrand = this.productArray[j].brand;
+          if (!(tempBrand in this.brandDict)) {
+            this.brandDict[tempBrand] = 1;
+          }
+          else {
+            this.brandDict[tempBrand] += 1;
+          }
+        }
         this.setPage(1);
+        this.spinner.hide();
       },
       (error) => {
         console.log(error);
@@ -61,58 +80,76 @@ export class StoreComponent implements OnInit {
     );
   }
 
-
   setPage(page: number) {
-    this.pager = this.pagerService.getPager(this.productArray.length, page);
-    this.displayUI = this.productArray.slice(this.pager.startIndex, this.pager.endIndex + 1);
-  }
-
-  changeCategory(event, item) {
-    console.log(event);
-    if (this.selectedCategory.includes(item)) {
-      this.selectedCategory = this.selectedCategory.filter(value => value != item);
-    }
-    else {
-      this.selectedCategory.push(item);
-    }
-    if (this.selectedCategory.length == 0) {
-      this.displayUI = this.productArray;
-    }
-    else {
-      this.displayUI = this.productArray.filter((event) => {
-        return this.selectedCategory.includes(event.category);
-      })
-    }
-  }
-
-  changeRange(e) {
-    if (this.selectedCategory.length == 0) {
-      this.displayUI = this.productArray.filter((item) => {
-        return (parseInt(item.price) >= parseInt(e.values[0]) && parseInt(item.price) <= parseInt(e.values[1]));
-      })
-    }
-    else {
-      this.displayUI = this.productArray.filter((event) => {
-        return (this.selectedCategory.includes(event.category) && (parseInt(event.price) >= parseInt(e.values[0]) && parseInt(event.price) <= parseInt(e.values[1])));
-      });
-    }
+    this.pager = this.pagerService.getPager(this.resultTemp.length, page);
+    this.displayUI = this.resultTemp.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 
   selectOption(id: number) {
-    console.log(this.selected);
     if (id == 1) {
-      this.productArray.sort(function (a, b) {
+      this.resultTemp.sort(function (a, b) {
         if (parseInt(a.price) > parseInt(b.price)) return -1;
         if (parseInt(a.price) < parseInt(b.price)) return 1;
       });
     }
     else {
-      this.productDisplay.sort(function (a, b) {
+      this.resultTemp.sort(function (a, b) {
         if (parseInt(a.price) < parseInt(b.price)) return -1;
         if (parseInt(a.price) > parseInt(b.price)) return 1;
       });
     }
-    this.displayUI = this.productArray;
+    this.setPage(1);
+  }
+
+  filterFunc(e, key?, type?) {
+    if (key === undefined) {
+      this.minRange = e.values[0];
+      this.maxRange = e.values[1];
+    }
+    else {
+      if (type == 'category') {
+        if (this.selectedCategory.includes(key)) {
+          this.selectedCategory = this.selectedCategory.filter(value => value != key);
+        }
+        else {
+          this.selectedCategory.push(key);
+        }
+      }
+      else if (type == "brand") {
+        if (this.selectedBrand.includes(key)) {
+          this.selectedBrand = this.selectedBrand.filter(value => value != key);
+        }
+        else {
+          this.selectedBrand.push(key);
+        }
+      }
+    }
+    console.log(this.selectedBrand);
+    this.resultArray();
+  }
+  resultArray() {
+    if (this.selectedBrand.length != 0 && this.selectedCategory.length == 0) {
+      this.displayUI = this.productArray.filter((item) => {
+        return (this.selectedBrand.includes(item.brand) && (parseInt(item.price) >= this.minRange) && parseInt(item.price) <= this.maxRange);
+      })
+    }
+    else if (this.selectedBrand.length == 0 && this.selectedCategory.length != 0) {
+      this.displayUI = this.productArray.filter((item) => {
+        return (this.selectedCategory.includes(item.category) && (parseInt(item.price) >= this.minRange) && parseInt(item.price) <= this.maxRange);
+      });
+    }
+    else if(this.selectedBrand.length != 0 && this.selectedCategory.length != 0){
+      this.displayUI = this.productArray.filter((item) => {
+        return (this.selectedBrand.includes(item.brand) && this.selectedCategory.includes(item.category) && (parseInt(item.price) >= this.minRange) && parseInt(item.price) <= this.maxRange);
+      })
+    }
+    else {
+      this.displayUI = this.productArray.filter((item) => {
+        return (parseInt(item.price) >= this.minRange && parseInt(item.price) <= this.maxRange);
+      })
+    }
+    this.resultTemp = this.displayUI;
+    this.setPage(1);
   }
 
 }
